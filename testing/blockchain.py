e28b43d50bec2c65d6ca60b9a2b60cbebcba79e8
jensen-lloyd <jl.110@protonmail.com>
2021-08-26 14:44:58 +1000

    Added mining blocks and automatical block creation once a time limit is reached. Also fixed some errors/bugs

--- testing/blockchain.py
+++ testing/blockchain.py
@@ -4,6 +4,8 @@ from datetime import timezone
 import hashlib
 
 
+
+
 def hash(*args):
     hash = ""
     for arg in args:
--- testing/blockchain_new.py
+++ testing/blockchain_new.py
@@ -4,6 +4,30 @@ import json
 from time import sleep
 
 
+import datetime
+from datetime import timezone
+import time
+
+
+
+
+#List of transactions that have been verified and are waiting to be added to block
+pending_transactions = []
+
+#Maximum amount of time (in minutes) that the oldest block will have to wait before being added to a block
+max_transaction_time = 0.1
+
+#Time oldest transaction has been waiting to be added to block
+transaction_countdown = 0
+
+#Mining difficulty
+difficulty = 3
+
+#Blockchain object/list
+blockchain = []
+
+
+
 
 #Function for easy hashing of any object or string
 def hash(*args):
@@ -12,17 +36,6 @@ def hash(*args):
         hash += str(arg)
 
     return str(hashlib.sha256(hash.encode('utf-8')).hexdigest())
-
-
-
-#List of transactions that have been verified and are waiting to be added to block
-pending_transactions = []
-
-
-
-class Block():
-    def __init__(self, index, pending_transactions):
-        self.timestamp = datetime.datetime.now(timezone.utc) 
 
 
 
@@ -57,17 +70,95 @@ def verify_transaction(transaction):
     pending_transactions.append(transaction)
 
 
+    global transaction_countdown
+    if transaction_countdown == 0:
+        transaction_countdown = time.time()
+
+
+
+
+
+
+
+
+class Block():
+
+    def __init__(self):
+        pass
+
+
+    def create_and_mine(self, data=pending_transactions):
+
+        self.block = {}
+        self.block_data = {}
+        self.transactions = {}
+        self.block_hash = {}
+
+        try:
+            self.block_data['index'] = int((blockchain[-1])['index']) + 1
+        except:
+            self.block_data['index'] = 0
+
+        try:
+            self.block_data['previous_hash'] = (blockchain[-1])['hash']
+        except:
+            self.block_data['previous_hash'] = "0" * 64
+
+
+        self.transactions = data
+
+
+        self.block['block_data'] = self.block_data
+        self.block['transactions'] = self.transactions
+
+        self.block_hash['nonce'] = 0
+        self.block_hash['hash'] = hash(str(self.block['block_data']) + str(self.block['transactions']))
+        while self.block_hash['hash'][:difficulty] != difficulty * "0":
+                
+                self.block_hash['hash'] = hash(str(self.block['block_data']) + str(self.block['transactions']) + str(self.block_hash['nonce']))
+
+                print("hash: " + self.block_hash['hash'])
+
+                self.block_hash['nonce'] +=1
+                print("nonce: " + str(self.block_hash['nonce']))
+
+
+
+        self.block['hash'] = self.block_hash 
+
+        return(json.dumps(self.block, indent=4))
+
+
+
+
+
 
 
 '''All the code needed to run (just runs other functions in a loop;
 taking in, validating and adding transactions from half_nodes or blocks from full_nodes)'''
 def main():
+
     while 2+2 == 4:
+
         verify_transaction(networking.listen_half_node())
-        print(pending_transactions[])
-        sleep(1)
+        sleep(10)
 
 
+        if (transaction_countdown) + int(max_transaction_time * 60) <= time.time():
+            #create block
+            block = Block()
+
+            print(Block().create_and_mine())
+
+            #mine block
+
+
+            #print block
+
+            #add block to blockchain
+
+
+        sleep(5)
 
 
 
